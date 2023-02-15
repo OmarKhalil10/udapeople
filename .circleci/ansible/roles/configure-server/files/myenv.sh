84ee6c5f0f3db67af0cf2172449ab39036dc088a
OmarKhalil10 <omar.khalil498@gmail.com>
2023-02-15 06:25:21 +0200

    update configure-server.yaml and add main.yaml and create a myenv.sh file inside another folder inside the configure-server folder named files, the env.sh file is made to carry all the evn vars to run on the target machine

---
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -0,0 +1,11 @@
+export TYPEORM_DATABASE=postgres
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=postgres
+export TYPEORM_HOST=database-2.cfhqlbcxgc68.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5432
+export TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export TYPEORM_MIGRATIONS_DIR=./src/migrations
+export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+
+
